e0c8c136ff82893da37bcc2c02ccc50be4de72af
ajaymarimuthu <ajayindia8001@gmail.com>
2023-01-24 21:51:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+ 
+import './App.css';
+import Article from './components/Articles';
+import Header from './components/Header';
+import Articles from './components/Articles';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="container">
+
+      <Header/>
+      <Articles/>
+      <Footer/>
+     
+    </div>
+  );
+}
+
+export default App;
